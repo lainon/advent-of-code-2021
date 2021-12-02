914fd653dcd1fce9d18b77c0b476625f7b2bbc21
lainon <lainon@protonmail.com>
2021-12-01 20:33:21 -0600

    Update main.py
    Generalized solution

--- day-1/main.py
+++ day-1/main.py
@@ -1,58 +1,71 @@
+import operator
 import os.path
 
 
-def part_one(file):
+def get_truth(a, relate, b):
+    ops = {
+        '>': operator.gt,
+        '<': operator.lt,
+        '>=': operator.ge,
+        '<=': operator.le,
+        '==': operator.eq,
+        '!=': operator.ne
+    }
+
+    return ops[relate](a, b)
+
+
+def load_data(file):
     file.seek(0)
     lines_in_file = file.readlines()
-    num_increased = 0
     data = []
 
     for line in lines_in_file:
         data.append(int(line))
 
-    for idx, item in enumerate(data):
-        if idx == 0:
-            continue
-        if data[idx] > data[idx - 1]:
-            num_increased += 1
-
-    print(f"part one: {num_increased}\n")
+    return data
 
 
-def part_two(file):
-    file.seek(0)
-    lines_in_file = file.readlines()
-    num_increased = 0
-    data = []
+def data_summation(data, window):
+    sums = []
 
-    for line in lines_in_file:
-        data.append(int(line))
+    for idx, d in enumerate(data):
+        s = 0
 
-    sliding_window_sums = []
-    for idx, item in enumerate(data):
-        sliding_window = 3
-        sum_current = 0
-
-        for x in range(sliding_window):
+        for x in range(window):
             if idx+x < len(data):
-                sum_current += data[idx+x]
+                s += data[idx+x]
 
-        sliding_window_sums.append(sum_current)
+        sums.append(s)
 
-    for idx, item in enumerate(sliding_window_sums):
+    return sums
+
+
+def reduction(data, operation):
+    result = 0
+
+    for idx, item in enumerate(data):
         if idx == 0:
             continue
-        if sliding_window_sums[idx] > sliding_window_sums[idx - 1]:
-            num_increased += 1
+        if get_truth(data[idx], operation, data[idx-1]):
+            result += 1
 
-    print(f"part two: {num_increased}")
+    return result
+
+
+def part_one(data):
+    print(f"part one: {reduction(data_summation(data, 1), '>')}")
+
+
+def part_two(data):
+    print(f"part two: {reduction(data_summation(data, 3), '>')}")
 
 
 def main():
     try:
         with open(os.path.join(os.path.dirname(__file__), "report.txt"), "r") as file:
-            part_one(file)
-            part_two(file)
+            part_one(load_data(file))
+            part_two(load_data(file))
     except IOError:
         print("File not found...")
 
